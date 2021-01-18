1fc50b1e64ede4549b3c7c227543e8a8a1039b3b
Devon Fazekas <thomas-d@hotmail.ca>
2021-01-17 23:05:54 -0500

    Implement Recommendations and Featured sections.

--- lib/screens/dashboard_page.dart
+++ lib/screens/dashboard_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_ordering/screens/food_tabs.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Dashboard extends StatefulWidget {
@@ -6,7 +7,16 @@ class Dashboard extends StatefulWidget {
   _DashboardState createState() => _DashboardState();
 }
 
-class _DashboardState extends State<Dashboard> {
+class _DashboardState extends State<Dashboard>
+    with SingleTickerProviderStateMixin {
+  TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(vsync: this, length: 4);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,8 +89,140 @@ class _DashboardState extends State<Dashboard> {
               ),
             ),
           ),
+          SizedBox(height: 20.0),
+          // Recommendations.
+          Padding(
+            padding: EdgeInsets.only(left: 15.0),
+            child: Text(
+              "Recommended",
+              style: GoogleFonts.notoSans(
+                  fontWeight: FontWeight.w500, fontSize: 18.0),
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Container(
+              height: 200.0,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  _buildFoodCard(
+                    iconUrl: "assets/burger.png",
+                    foodName: "Hamburger",
+                    price: "21",
+                    boxColor: Color(0xFFFFE9C6),
+                    txtColor: Color(0xFFDA9551),
+                  ),
+                  _buildFoodCard(
+                    iconUrl: "assets/fries.png",
+                    foodName: "Fries",
+                    price: "15",
+                    boxColor: Color(0xFFC3E3FF),
+                    txtColor: Color(0xFF7799B9),
+                  ),
+                  _buildFoodCard(
+                    iconUrl: "assets/doughnut.png",
+                    foodName: "Doughnut",
+                    price: "15",
+                    boxColor: Color(0xFFD7FBD9),
+                    txtColor: Color(0xFF4DCD76),
+                  ),
+                ],
+              )),
+          // Featured.
+          SizedBox(height: 20.0),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0),
+            child: TabBar(
+              controller: tabController,
+              tabs: [
+                Tab(child: Text("FEATURED")),
+                Tab(child: Text("COMBO")),
+                Tab(child: Text("FAVOURITES")),
+                Tab(child: Text("RECOMMENDED")),
+              ],
+              isScrollable: true,
+              indicatorColor: Colors.transparent,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.withOpacity(0.5),
+              labelStyle: GoogleFonts.notoSans(
+                  fontSize: 16.0, fontWeight: FontWeight.w700),
+              unselectedLabelStyle: GoogleFonts.notoSans(
+                  fontSize: 12.0, fontWeight: FontWeight.w500),
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height - 450.0,
+            child: TabBarView(
+              controller: tabController,
+              children: [
+                FoodTab(),
+                FoodTab(),
+                FoodTab(),
+                FoodTab(),
+              ],
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  Widget _buildFoodCard({
+    String iconUrl,
+    String foodName,
+    String price,
+    Color boxColor,
+    Color txtColor,
+  }) {
+    return Padding(
+        padding: EdgeInsets.only(left: 15.0),
+        child: InkWell(
+          onTap: () {},
+          child: Container(
+            height: 175.0,
+            width: 150.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              color: boxColor,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Item icon.
+                Hero(
+                  tag: foodName,
+                  child: Container(
+                    height: 75.0,
+                    width: 75.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      child: Image.asset(iconUrl, height: 50.0, width: 50.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                // Item name.
+                Text(
+                  foodName,
+                  style: TextStyle(
+                    fontSize: 17.0,
+                    color: txtColor,
+                  ),
+                ),
+                // Item price.
+                Text(
+                  "\$" + price,
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: txtColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
---
+++ lib/screens/food_tabs.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FoodTab extends StatefulWidget {
+  @override
+  _FoodTabState createState() => _FoodTabState();
+}
+
+class _FoodTabState extends State<FoodTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          _buildListItem(
+              foodName: "Delicious hot dog",
+              rating: 4.0,
+              afterPrice: '6',
+              beforePrice: '10',
+              imgPath: 'assets/hotdog.png'),
+          _buildListItem(
+              foodName: "Cheese pizza",
+              rating: 5.0,
+              afterPrice: '12',
+              beforePrice: '14',
+              imgPath: 'assets/hotdog.png'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListItem(
+      {String foodName,
+      double rating,
+      String afterPrice,
+      String beforePrice,
+      String imgPath}) {
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          // Left column -- image section.
+          Container(
+            width: 110.0,
+            child: Row(children: [
+              Container(
+                  height: 75.0,
+                  width: 75.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7.0),
+                    color: Color(0xFFFFE3DF),
+                  ),
+                  child: Center(
+                      child: Image.asset(imgPath, height: 50.0, width: 50.0))),
+            ]),
+          ),
+          // Middle column -- content section.
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // Food name.
+              Text(
+                foodName,
+                style: GoogleFonts.notoSans(
+                    fontSize: 14.0, fontWeight: FontWeight.w400),
+              ),
+              // Food rating.
+              SmoothStarRating(
+                allowHalfRating: false,
+                onRated: (v) {},
+                starCount: rating.toInt(),
+                rating: rating,
+                color: Color(0xFFFFD143),
+                borderColor: Color(0xFFFFD143),
+                size: 15.0,
+                spacing: 0.0,
+                isReadOnly: true,
+              ),
+              // Food price.
+              Row(
+                children: <Widget>[
+                  // New price.
+                  Text(
+                    '\$' + afterPrice,
+                    style: GoogleFonts.lato(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      textStyle: TextStyle(
+                        color: Color(0xFFF68D7F),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 3.0),
+                  // Old price.
+                  Text(
+                    '\$' + beforePrice,
+                    style: GoogleFonts.lato(
+                      fontSize: 12.0,
+                      decoration: TextDecoration.lineThrough,
+                      fontWeight: FontWeight.w600,
+                      textStyle: TextStyle(
+                        color: Colors.grey.withOpacity(0.4),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          // Right column -- button section.
+          FloatingActionButton(
+            heroTag: foodName,
+            mini: true,
+            onPressed: () {},
+            child: Center(
+              child: Icon(Icons.add, color: Colors.white),
+            ),
+            backgroundColor: Color(0xFFFE7D6A),
+          ),
+        ],
+      ),
+    );
+  }
+}
